f385a672a0be5612e5730645b9327758459685ec
jensaronsson <jensarons@gmail.com>
2021-09-21 21:45:01 +0200

    Use redirecturi from environment

--- config.ts
+++ config.ts
@@ -2,6 +2,9 @@ export default {
   tink: {
     clientId: process.env.NEXT_PUBLIC_TINK_CLIENT_ID,
     clientSecret: process.env.TINK_CLIENT_SECRET,
-    redirectUri: "http://localhost:3000/callback",
+    redirectUri:
+      process.env.NEXT_PUBLIC_VERCEL_URL ??
+      process.env.TINK_LINK_REDIRECT_URI ??
+      "http://localhost:3000/callback",
   },
 };
