cc0dcd3619902fb3acb118aab3fa37084728591b
jensaronsson <jensarons@gmail.com>
2021-09-21 21:51:24 +0200

    Use custom env instead

--- config.ts
+++ config.ts
@@ -3,8 +3,6 @@ export default {
     clientId: process.env.NEXT_PUBLIC_TINK_CLIENT_ID,
     clientSecret: process.env.TINK_CLIENT_SECRET,
     redirectUri:
-      process.env.NEXT_PUBLIC_VERCEL_URL ??
-      process.env.TINK_LINK_REDIRECT_URI ??
-      "http://localhost:3000/callback",
+      process.env.TINK_LINK_REDIRECT_URI ?? "http://localhost:3000/callback",
   },
 };
