881521c10ff82f64358d26fffb33851f64f5a798
jensaronsson <jensarons@gmail.com>
2021-10-02 15:35:30 +0200

    Add padding to layout

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar";
 
 function MainLayout(props: { children: ReactNode }) {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-4">
       <Navbar />
       <main>{props.children}</main>
     </div>
