de02984d1a64bbf228fcdb6f038b7fc0a969e80e
jensaronsson <jensarons@gmail.com>
2021-11-09 22:52:30 +0100

    Fix: expose redirect uri in both client and server all times.
    Since we moved `getTinkLinkUrl` from only been rendered on server to
    also being renderd on the client. We ended up with a undefined value
    from that resulted in fallback to localhost.
    Prefix with NEXT_PUBLIC_ will solve this.

--- config.ts
+++ config.ts
@@ -1,9 +1,12 @@
-export default {
+const tinkConfig = {
   tink: {
     apiBaseUrl: "https://api.tink.com/",
     clientId: process.env.NEXT_PUBLIC_TINK_CLIENT_ID,
     clientSecret: process.env.TINK_CLIENT_SECRET,
     redirectUri:
-      process.env.TINK_LINK_REDIRECT_URI ?? "http://localhost:3000/callback",
+      process.env.NEXT_PUBLIC_TINK_LINK_REDIRECT_URI ??
+      "http://localhost:3000/callback",
   },
 };
+
+export default tinkConfig;
