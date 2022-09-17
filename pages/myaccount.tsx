016da91314ff334f601e95c5e32a12663d43e98c
Jens Aronsson <jensarons@gmail.com>
2022-09-17 17:52:20 +0200

    Increased type-safety a bit to infer as much as we can.

--- pages/myaccount.tsx
+++ pages/myaccount.tsx
@@ -3,35 +3,27 @@ import AccountComponent from "../components/myAccountContainerComps/AccountCompo
 import SavingsComponent from "../components/myAccountContainerComps/SavingsComponent";
 import TransferComponent from "../components/myAccountContainerComps/TransferComponent";
 
-interface Comps {
-  account: React.ElementType;
-  savings: React.ElementType;
-  transfer: React.ElementType;
-}
+const sections = {
+  account: AccountComponent,
+  savings: SavingsComponent,
+  transfer: TransferComponent,
+} as const;
 
-interface Style {
-  default: string;
-}
-
+const styles = {
+  default:
+    "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium",
+  active:
+    "border-terra-purple-600 text-terra-purple-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium",
+};
 function MyAccount() {
-  const sections: Comps = {
-    account: AccountComponent,
-    savings: SavingsComponent,
-    transfer: TransferComponent,
-  };
-
   const sectionNames = Object.keys(sections);
-  const [activeSection, setActiveSection] = useState("account");
-  const SectionComponent =
-    sections[activeSection as keyof Comps] || sections.account;
+  const [activeSection, setActiveSection] =
+    useState<keyof typeof sections>("account");
+  const SectionComponent = sections[activeSection] || sections.account;
 
-  const styles: Style = {
-    default:
-      "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 w-1/4 py-4 px-1 text-center border-b-2 font-medium",
+  const defaultProps = {
+    value: 1,
   };
-
-  styles[activeSection as keyof Style] =
-    "border-terra-purple-600 text-terra-purple-600 w-1/4 py-4 px-1 text-center border-b-2 font-medium";
 
   return (
     <div className="lg:mx-72 md:mx-10 mx-4 mt-10">
@@ -39,7 +31,9 @@ function MyAccount() {
         {sectionNames.map((section) => (
           <button
             key={section}
-            className={styles[section as keyof Style] || styles.default}
+            className={
+              section === activeSection ? styles.active : styles.default
+            }
             onClick={() => setActiveSection(section)}
           >
             {section}
@@ -47,7 +41,7 @@ function MyAccount() {
         ))}
       </div>
 
-      <SectionComponent />
+      <SectionComponent {...defaultProps} />
     </div>
   );
 }
